80df8d05ec12bea1ee707faf8668fa6b1b301ab2
Haci Murat Yaman <muratyaman@gmail.com>
2026-03-13 23:05:29 +0300

    initial version

--- mock-app/src/index.ts
+++ mock-app/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { randomUUID } from 'node:crypto';
-import { makeAxiosFactory } from '@rataqa/jalb';
+import { randomInt, randomUUID } from 'node:crypto';
+import { IAxiosFactory, makeAxiosFactory } from '@rataqa/jalb';
 import { makeLogger, type IBasicLogger } from '@rataqa/sijil';
 
 const app = express();
@@ -9,17 +9,20 @@ const appInfo = { appName: 'mock-app', appVersion: '1.2.3' };
 
 const logger = makeLogger('pino', appInfo, { level: 'info' });
 
-const exampleDotCom = makeAxiosFactory('http://example.com', { headers: { 'x-api-key': '12345' } }, logger.defaultLogger);
+const exampleDotCom = makeAxiosFactory('http://example.com', { headers: { 'x-api-key': '12345' } });
 
 const serviceA = makeServiceA(exampleDotCom);
 
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.locals.t0 = new Date();
+  const t0 = new Date();
 
   const correlation_id = req.get('x-correlation-id') || randomUUID();
-  res.locals.correlation_id = correlation_id;
+  res.locals.ctx = {
+    t0,
+    correlation_id,
+  };
 
   const rl = logger.makeLoggerPerRequest({ correlation_id });
 
@@ -34,18 +37,19 @@ app.get('/', (_req: Request, res: Response) => {
   res.json({ data: appInfo, ts: new Date() });
 });
 
-app.post('/', (req: IRequest, res: IResponse) => {
+app.post('/', async (req: IRequest, res: IResponse) => {
   const { ctx, reqLogger } = res.locals;
+  const { lat = randomInt(100), lon = randomInt(100) } = req.body;
   reqLogger.info('Handling request for root path');
 
   const api = serviceA.apiPerRequest(ctx, reqLogger);
-  const result = api.homePage();
+  const result = await api.homePage(lat, lon);
 
   res.send(result);
 });
 
 app.listen(3000, () => {
-  logger.info('Server is running on port 3000');
+  logger.defaultLogger.info('Server is running on port 3000');
 });
 
 interface IInputToGet {
@@ -70,13 +74,14 @@ interface IAppCtx {
 function makeServiceA(ax: IAxiosFactory) {
 
   function apiPerRequest(ctx: IAppCtx, rl: IBasicLogger) {
-    
+
     const http = ax.makeAxiosPerRequest({ 'x-correlation-id': ctx.correlation_id }, rl);
-    
+
     async function homePage(lat: number, lon: number) {
       rl.info('homePage()', { lat, lon });
       const result = await http.get('/');
-      return result;
+
+      return result.data;
     }
 
     return {
